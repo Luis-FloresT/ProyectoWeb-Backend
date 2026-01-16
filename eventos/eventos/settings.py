82d528cfd43985b9750eec356cb099c513aa98a1
Paulpc13 <you@example.com>
2026-01-16 13:57:23 -0500

    chore: ruta de verificacion de contraseña

--- eventos/eventos/settings.py
+++ eventos/eventos/settings.py
@@ -147,7 +147,7 @@ ALLOWED_HOSTS = ['*']
 CSRF_TRUSTED_ORIGINS = [
     "https://*.ngrok-free.dev",
     "https://*.vercel.app",
-    "https://proyectoweb-fronted.vercel.app",  # Nueva URL Oficial
+    "https://proyectweb-fronted.vercel.app",  # URL Corregida por Usuario
     "http://localhost:5173",
     "http://127.0.0.1:5173",
 ]
@@ -168,5 +168,5 @@ STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 # URL del Backend (Dinámica para Ngrok/Render)
 BACKEND_URL = env('BACKEND_URL', default='https://melina-dynastical-shenita.ngrok-free.dev')
 
-# URL del Frontend (Oficial)
-FRONTEND_URL = env('FRONTEND_URL', default='https://proyectoweb-fronted.vercel.app')
+# URL del Frontend (Oficial - Corregida)
+FRONTEND_URL = env('FRONTEND_URL', default='https://proyectweb-fronted.vercel.app')
